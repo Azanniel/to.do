24037f12da1e8678ceb16e20d371d3b9d84b5502
azanniel <leo.azannielttt@gmail.com>
2022-05-30 22:01:31 -0400

    alert when task iguals another task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -9,6 +9,13 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    if(tasks.find(item => item.title === newTaskTitle.trim())) {
+      return Alert.alert(
+        'Task já cadastrada', 
+        'Você não pode cadastrar uma task com o mesmo nome'
+      );
+    }
+
     const data: Task = {
       id: new Date().getTime(),
       title: newTaskTitle,
